c381fa17f640efdd587c0888084af7eb4307e990
Jun Orville Lecena <jun.lecena@comptrolla.com>
2024-01-07 02:50:03 +0800

    updated trpc deps version

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,4 @@
-import { Elysia, t } from 'elysia'
+import { Elysia } from 'elysia'
 import { cors } from '@elysiajs/cors'
 import { staticPlugin } from '@elysiajs/static'
 import { swagger } from '@elysiajs/swagger'
